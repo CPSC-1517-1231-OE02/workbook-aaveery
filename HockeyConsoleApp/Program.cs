cefd9dda06f427a470d2be490c14b4b865f71964
aaveery <112987036+aaveery@users.noreply.github.com>
2023-09-13 21:56:54 -0600

    added utilities class and started unit testing

---
+++ HockeyConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using HockeyClassLibrary.Data;
+
+Console.WriteLine("Welcome to the HockeyPlayer Test App");
+
+// initializing an object using the default constructor
+HockeyPlayer player1 = new HockeyPlayer();
+player1.FirstName = "Stewart";
+player1.LastName = "Skinner";
+
+// object-initializer - initializaing some of the properties and using default constructor values for the rest of them
+HockeyPlayer player2 = new HockeyPlayer()
+{
+    FirstName = "Connor",
+    LastName = "McDavid"
+};
+
+// initializing an object using the greedy constructor
+HockeyPlayer player3 = new HockeyPlayer("Bobby", "Orr", "Parry Sound", new DateOnly(1948, 03, 20), 196, 73, Position.Defense, Shot.Right);
+
+// output things bout the playerrs
+Console.WriteLine($"The player's name is {player1.FirstName} {player1.LastName} and they are born on {player1.DateOfBirth}.");
+Console.WriteLine($"The player's name is {player2.FirstName} {player2.LastName} and they are born on {player2.DateOfBirth}.");
+Console.WriteLine($"The player's name is {player3.FirstName} {player3.LastName} and they are born on {player3.DateOfBirth}.");
---
+++ HockeyTestProject/HockeyPlayerTest.cs
@@ -0,0 +1,42 @@
+﻿using HockeyClassLibrary.Data;
+using FluentAssertions;
+
+namespace Hockey.Test;
+
+public class HockeyPlayerTest
+{
+    // makes a new hockey player instance so that we can have an object to test our data on 
+    public HockeyPlayer GenerateTestPlayer()
+    {
+        return new HockeyPlayer();
+    }
+
+    [Fact]
+    public void HockeyPlayer_FirstName_ReturnsGoodFirstName()
+    {
+        // arrange - defining values
+        HockeyPlayer player = GenerateTestPlayer();
+        const string NAME = "test";
+        player.FirstName = NAME;
+
+        // act - what should the test be equivalent to?
+        string actual = player.FirstName;
+
+        // assert - the actual test
+        actual.Should().Be(NAME);
+    }
+
+    [Fact]
+    public void HockeyPlayer_FirstName_ThrowsExceptionForEmptyArg()
+    {
+        // arrange
+        HockeyPlayer player = GenerateTestPlayer();
+        const string EMPTY_NAME = "";
+
+        // act
+        Action act = () => player.FirstName = EMPTY_NAME;
+
+        // assert
+        act.Should().Throw<ArgumentException>().WithMessage("First Name cannot be null or empty.");
+    }
+}
